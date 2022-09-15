0e2e21227fb8ab3a89601a1f3aa960043122935c
Gabriel Puttin <gabrielputtin@hotmail.com>
2022-09-15 15:11:56 -0300

    Finished exercise

--- Front-end/Bloco-17/Dia-01/context-color-box/src/App.js
+++ Front-end/Bloco-17/Dia-01/context-color-box/src/App.js
@@ -1,25 +1,11 @@
-import { Component } from 'react';
+import './App.css';
+import React from 'react';
 import ColorBox from './components/ColorBox';
-import MyContext from './MyContext';
 
-class App extends Component {
-  state = {
-    background: ['blue', 'red', 'yellow'],
-  };
-
-  changeBackground = () => {
-    const { background } = this.state;
-    console.log(background.reduce((acc, curr) => acc + curr, []));
-  }
-
+class App extends React.Component {
   render() {
-    return(
-      <div>
-        <MyContext.Provider value={this.state.background}>
-          <ColorBox />
-        </MyContext.Provider>
-        <button type='button' onClick={this.changeBackground}>Entrar</button>
-      </div>
+    return (
+      <ColorBox />
     );
   }
 }
--- Front-end/Bloco-17/Dia-01/context-color-box/src/components/ColorBox.jsx
+++ Front-end/Bloco-17/Dia-01/context-color-box/src/components/ColorBox.jsx
@@ -1,18 +1,25 @@
-import React, { Component } from "react";
+// ./components/ColorBox.js
 
-class ColorBox extends Component {
+import React from 'react';
+import ColorsContext from '../context/ColorsContext';
+import '../styles/box.css';
+
+class ColorBox extends React.Component {
   render() {
+    const { nextColor, colors, colorIndex } = this.context;
     return (
-      <div>
-        <button
-          type="button"
-          className="box"
-          style={{ backgroundColor: 'blue' }}
-        >
-          Click me to change my color!
-        </button>
-      </div>);
+      <button
+        type="button"
+        className="box"
+        style={ { backgroundColor: colors[colorIndex] } }
+        onClick={ () => nextColor() }
+      >
+        Change my color!
+      </button>
+    );
   }
 }
+
+ColorBox.contextType = ColorsContext;
 
 export default ColorBox;
