445e94d838d0eaf754d5b1528bab0617220bec30
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-04-11 15:23:15 -0300

    Feat: add scraping example with selenium

---
+++ Ciência_Da_Computação/Bloco-03/Dia-02/selenium_example.py
@@ -0,0 +1,24 @@
+# from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys # Importa teclas comuns
+
+
+options = webdriver.FirefoxOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--start-maximized')
+
+firefox = webdriver.Remote(command_executor="http://localhost:4444/wd/hub",
+                           options=options)
+
+response = firefox.get("https://www.google.com.br/")
+
+# pesquisa na instância aberta do navegador pela primeira ocorrência
+# do nome 'q'
+search_input = firefox.find_element("name", "q")
+
+# escreve selenium dentro do campo de pesquisa
+search_input.send_keys("selenium")
+
+# pressiona enter para realizar a busca
+search_input.send_keys(Keys.ENTER)
