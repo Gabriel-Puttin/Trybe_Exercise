301d67719d839e62508f0c0cb4dd80795f2475fb
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-10-20 15:52:35 -0300

    Feat: build ConcatString and SplitString example func

---
+++ CSharp/Bloco-01/Dia-04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main()
+        {
+            ConcatString();
+        }
+
+        public static void ConcatString()
+        {
+            string textOne = "Você está aprendendo sobre ";
+            string textTwo = "Strings em C#, ";
+            string textThree = "e agora sabe concatenar textos utilizando a função Concat()!";
+
+            string concatResult = string.Concat(textOne, textTwo, textThree);
+            Console.WriteLine(concatResult);
+        }
+
+        public static void SplitString()
+        {
+            string emails = "email1@trybe.com;email2@trybe.com;email3@trybe.com";
+            string[] arrayEmails = emails.Split(";");
+            Console.WriteLine(arrayEmails);
+        }
+    }
+}
