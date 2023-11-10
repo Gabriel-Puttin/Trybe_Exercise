229164e948a5b2bcf17511f03be4af952130734b
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-11-10 17:43:02 -0300

    Feat: created post client route

---
+++ CSharp/Bloco-03/Dia-01/api/Controllers/ClientController.cs
@@ -0,0 +1,20 @@
+namespace api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using api.Core;
+
+[ApiController]
+[Route("clients")]
+public class ClientController : ControllerBase
+{
+    private static List<Client> _clients = new();
+    private static int _nextId = 1;
+
+    [HttpPost]
+    public ActionResult Create(ClientRequest request)
+    {
+        var client = request.CreateClient(_nextId++);
+        _clients.Add(client);
+
+        return StatusCode(201, client);
+    }
+}
---
+++ CSharp/Bloco-03/Dia-01/api/Core/ClientRequest.cs
@@ -0,0 +1,19 @@
+namespace api.Core;
+
+public class ClientRequest
+{
+    public string? Name { get; set; }
+    public decimal AccountBalance { get; set; }
+
+    public Client CreateClient(int id)
+    {
+        return new Client
+        {
+            Id = id,
+            Name = Name,
+            AccountBalance = AccountBalance,
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now
+        };
+    }
+}
