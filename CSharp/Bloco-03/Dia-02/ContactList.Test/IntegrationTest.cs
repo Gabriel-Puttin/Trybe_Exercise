4bd5aecdf1e71ebba1f274c58f65b527fc131433
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-11-22 16:10:32 -0300

    Chore: add dependencies

---
+++ CSharp/Bloco-03/Dia-02/ContactList.Test/IntegrationTest.cs
@@ -0,0 +1,10 @@
+namespace ContactList.Test;
+
+using Moq;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using ContactList.Models;
+using ContactList.Services;
+using ContactList.Controllers;
