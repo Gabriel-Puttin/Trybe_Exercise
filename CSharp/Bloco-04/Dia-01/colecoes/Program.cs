4a22ed76e219e96a10aa6bdab2853fa0300d6560
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-11-29 15:29:24 -0300

    Feat: learning colections in C#

---
+++ CSharp/Bloco-04/Dia-01/colecoes/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace colecoes;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> vehicles = new List<string>(){ "moto", "carro", "avião" };
+        vehicles.Add("navio");
+        vehicles.Remove("navio");
+        vehicles.RemoveAt(2);
+        vehicles.Sort();
+
+        foreach (var item in vehicles)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine(vehicles.IndexOf("moto"));
+
+        
+    }
+}
