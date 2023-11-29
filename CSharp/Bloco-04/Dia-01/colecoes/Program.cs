ab2efbe3a70a31ae990feba2e1801664d4a828c9
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-11-29 15:42:22 -0300

    Feat: examples using LINQ

--- CSharp/Bloco-04/Dia-01/colecoes/Program.cs
+++ CSharp/Bloco-04/Dia-01/colecoes/Program.cs
@@ -1,9 +1,15 @@
 ﻿namespace colecoes;
+
+internal class Artist
+{
+    public string? name { get; set; }
+    public int listeners { get; set; }
+}
 public class Program
 {
     public static void Main(string[] args)
     {
-        List<string> vehicles = new List<string>(){ "moto", "carro", "avião" };
+        List<string> vehicles = new List<string>() { "moto", "carro", "avião" };
         vehicles.Add("navio");
         vehicles.Remove("navio");
         vehicles.RemoveAt(2);
@@ -15,6 +21,33 @@ public class Program
         }
         Console.WriteLine(vehicles.IndexOf("moto"));
 
-        
+        string[] games = { "Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft" };
+
+        IEnumerable<string> filteredGames = from game in games
+                                            where game.Contains('a')
+                                            select game;
+        foreach (var game in filteredGames)
+        {
+            Console.WriteLine(game);
+        }
+
+        var artists = new List<Artist>
+        {
+            new Artist { name = "Raul Seixas", listeners = 50000 },
+            new Artist { name = "Mozart", listeners = 15000 },
+            new Artist { name = "Elvis Presley", listeners = 25000 },
+            new Artist { name = "Bob Dylan", listeners = 30000 },
+            new Artist { name = "Guns N' Roses", listeners = 40000 },
+        };
+
+        var topListeners =
+               from artist in artists
+               where artist.listeners > 30000
+               select artist.name;
+
+        foreach (var artist in topListeners)
+        {
+            Console.WriteLine(artist);
+        }
     }
 }
