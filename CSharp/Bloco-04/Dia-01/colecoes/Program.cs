553eb430086ec3b5f4de22675b4991ea0b7ac0e4
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-11-29 16:42:27 -0300

    Feat: examples using DTOs

--- CSharp/Bloco-04/Dia-01/colecoes/Program.cs
+++ CSharp/Bloco-04/Dia-01/colecoes/Program.cs
@@ -49,5 +49,41 @@ public class Program
         {
             Console.WriteLine(artist);
         }
+        List<Customer> customers = new List<Customer>
+        {
+            new Customer { Id = 1, Name = "Rebeca", City = "Recife", Password =  "706632"},
+            new Customer { Id = 2, Name = "José", City = "Manaus", Password =  "128620"},
+            new Customer { Id = 3, Name = "Sandra", City = "Salvador", Password =  "043407"}
+        };
+        var customersList = from customer in customers
+                            select new CustomerBasicData { Name = customer.Name, City = customer.City };
+
+        foreach (CustomerBasicData customer in customersList)
+        {
+            Console.WriteLine(customer.Name + " - City: " + customer.City);
+        }
+
+        List<Author> authors = new List<Author>
+        {
+            new Author { Id = 1, Name = "Clarice Lispector"},
+            new Author { Id = 2, Name = "Jorge Amado" }
+        };
+        List<Book> books = new List<Book>
+        {
+            new Book { Name = "Capitães da Areia", AuthorId = 2},
+            new Book { Name = "Água Viva", AuthorId = 1},
+            new Book { Name = "A hora da Estrela", AuthorId = 1},
+            new Book { Name = "Cacau", AuthorId = 2},
+        };
+        var booksList = from book in books
+                        from author in authors
+                        where author.Id == book.AuthorId
+                        orderby author.Name
+                        select new BookDTO { BookName = book.Name, AuthorName = author.Name };
+
+        foreach (BookDTO book in booksList)
+        {
+            Console.WriteLine(book.BookName + " - Author: " + book.AuthorName);
+        }
     }
 }
