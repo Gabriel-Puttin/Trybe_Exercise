7038f7d28e7bb6a0c5a20f50f73d8cc3f7ef10c0
Gabriel Puttin <gabrielputtin@hotmail.com>
2024-02-13 19:06:13 -0300

    Feat: build user controller

--- CSharp/Bloco-06/Dia-01/jwt_api/Controllers/UserController.cs
+++ CSharp/Bloco-06/Dia-01/jwt_api/Controllers/UserController.cs
@@ -1,21 +1,35 @@
 using jwt_api.Models;
+using jwt_api.Repository;
+using jwt_api.Services;
 using jwt_api.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace jwt_api.Controllers;
 
-[HttpPost]
-
-public ActionResult<UserViewModel> Authenticate([FromBody] User user)
+public class AuthController : Controller
 {
-    UserViewModel userViewModel = new UserViewModel();
-    try
+    [HttpPost]
+    [Route("authenticate")]
+    [AllowAnonymous]
+    public ActionResult<UserViewModel> Authenticate([FromBody] User user)
     {
+        UserViewModel userViewModel = new UserViewModel();
+        try
+        {
+            userViewModel.User = new UserRepository().Get(user);
+            if (userViewModel.User == null)
+            {
+                return NotFound("User not found");
+            }
+            userViewModel.Token = new TokenGenerator().Generate();
+            // userViewModel.User.Password = string.Empty;
 
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        return userViewModel;
     }
-    catch
-    {
-
-    }
-    return userViewModel;
 }
--- CSharp/Bloco-06/Dia-01/jwt_api/Program.cs
+++ CSharp/Bloco-06/Dia-01/jwt_api/Program.cs
@@ -1,3 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -17,6 +21,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
