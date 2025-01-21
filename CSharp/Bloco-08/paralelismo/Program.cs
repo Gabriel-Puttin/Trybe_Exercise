65db271ffe050e5ce65075375c90509c5d4fdd36
Gabriel Puttin <gabrielputtin@hotmail.com>
2025-01-21 19:51:36 -0300

    Paralelismo em C#

---
+++ CSharp/Bloco-08/paralelismo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class SayHello {
+    public static void SaySyncHello() {
+        Thread.Sleep(10000);
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static async Task SayAsyncHello() {
+        Thread.Sleep(1000);
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static void Main(){
+        Console.WriteLine("Instrução 1");
+        Thread.Sleep(500);
+        Console.WriteLine("Instrução 2");
+        Thread.Sleep(500);
+        Console.WriteLine("Instrução 3");
+        Thread.Sleep(500);
+
+        // SaySyncHello();
+        Task.Run(() => SayAsyncHello());
+    
+        Console.WriteLine("Instrução 4");
+        Thread.Sleep(500);
+        Console.WriteLine("Instrução 5");
+        Thread.Sleep(500);
+        Console.WriteLine("Instrução 6");
+    }
+}
